3c816aa829372d32fea13ab7d63c3bd25da501f3
GayatriSaykar <134963806+GayatriSaykar@users.noreply.github.com>
2024-03-04 22:31:47 +0530

    Initially Committed

---
+++ Frontend/eathub/src/components/Register.js
@@ -0,0 +1,13 @@
+export default function Register()
+{
+    return(
+        <div>
+            <form>
+            <h2 style={{textAlign:"center"}}>Registration</h2>
+            <div className="mb-2">
+            <label className="form-label" >Name:</label>
+            </div>
+            </form>
+        </div>
+    )
+}
